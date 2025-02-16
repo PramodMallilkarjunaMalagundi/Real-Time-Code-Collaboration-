22f7e88adc361ee7cdf429ba1f1d56b0f3dc7ee0
Pramod Mallikarjuna Malagundi <153420761+PramodMallilkarjunaMalagundi@users.noreply.github.com>
2025-02-16 15:16:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = `https://realtime-code-editor-final.onrender.com`;
+const url = `https://real-time-code-collaboration-and-code.onrender.com`;
 const interval = 30000;
 
 function reloadWebsite() {
